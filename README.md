b23d1954392594c2c76f5e91e4f8a5323b30c7bb
Nuno Santos <42045537+nunosantoswebdesigner@users.noreply.github.com>
2019-08-01 18:06:28 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# autocomplete
+### ShareI-tshirts
+
+#### Exercício:
+    
+    - 1. Criar página em HTML + CSS3 + Javascript com o seguinte aspecto gráfico:
+    - 2. Os items listados deverão ser obtidos através de um Json que se encontra na pasta deste exercício (products.json).
+    - 3. A caixa de pesquisa deverá despoletar o preenchimento e, a população da listagem, a partir da terceira letra / auto-complete. Antes de qualquer pesquisa, não deverão ser apresentados quaisquer resultados.
+    - 4. O “total de resultados” deverá ser dinâmico em função dos items obtidos.
+
+#### Nota:
+    Mapeamento entre o Json e o resultado no front-end: 
+
+
---
+++ scripts/app.js
@@ -0,0 +1,125 @@
+const search = document.getElementById('search')
+const matchList = document.getElementById('match-list')
+const matchCounter = document.getElementById('match-counter-container')
+const form = document.getElementById('form')
+const iconSearch = document.getElementById('icon-search')
+// const autocomplete = document.getElementById('autocomplete');
+const container = document.querySelector('#autocomplete-container');
+const autocomplete = container.querySelector('#autocomplete');
+const mainInput = container.querySelector('#search');
+
+
+
+
+// f() async para procurar produtos no json 
+const searchTshirts = async searchText => {
+    //Usei fectch API para fazer o request ao json //https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API/Using_Fetch
+    //https://caniuse.com/#feat=fetch
+    //.json() para o formato da resposta ref: https://developer.mozilla.org/en-US/docs/Web/API/Body/json
+    const res = await fetch('../data/products.json')
+    const products = await res.json();
+    let tshirts = products.items;
+
+    // para fazer corresponder o valor do input aos items do json
+    // Usei o filter method para percorrer o array e devolver novo array com as correspondencias
+    let matches = tshirts.filter(tshirt => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return tshirt.title.match(regex);
+    })
+
+    // controlo de digitos 
+    if (searchText.length === 0) {
+        matches = [];
+        // forçar o match apartir do 3º caractere
+    } else if (searchText.length < 3) {
+        matches = [];
+    } else {
+        setMatchCounter(matches);
+    }
+    // f () call
+    setTitleAutocomplete(tshirts)
+    outputHtml(matches);
+}
+
+// f() para calcular n de matchs
+const setMatchCounter = matches => {
+    console.log(matches.length)
+    // teste ternario para controlar plural no texto do resultado
+    const html = `<span class="counter-result">${matches.length  + ' ' + (matches.length == 1 ? 'Resultado' : 'Resultados') }</span>`
+    matchCounter.innerHTML = html;
+}
+
+// f() para executar o autocomplete
+// criar um array vazio para guardar todos os titulos para depois comparar e fazer o auto complete
+const setTitleAutocomplete = (tshirts) => {
+    let foundTitle = '';
+    let titles = [];
+    tshirts.forEach(element => {
+        titles.push(element.title)
+    });
+
+    search.addEventListener('keyup', onKeyUp);
+
+    function onKeyUp(e) {
+        if (search.value === '') {
+            autocomplete.textContent = '';
+            return;
+        }
+
+        if (keyChecker(e, 'Enter') || keyChecker(e, 'ArrowRight')) {
+            search.value = foundTitle;
+            autocomplete.textContent = '';
+        }
+
+        for (let word of titles) {
+            if (word.indexOf(search.value) === 0) {
+                foundTitle = word;
+                autocomplete.textContent = word;
+                break;
+            } else {
+                foundTitle = '';
+                autocomplete.textContent = '';
+            }
+        }
+    }
+
+    function keyChecker(e, key) {
+        const keys = {
+            'ArrowRight': 37,
+            'Enter': 13,
+            'ArrowLeft': 39
+        }
+
+        if (e.keyCode === keys[key] || e.which === keys[key] || e.key === key) return true;
+        return false;
+    }
+}
+
+// f para colocar as matches no html
+const outputHtml = matches => {
+    if (matches.length > 0) {
+        const html = matches
+            .map(
+                // Template String ref:https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/template_strings
+                match => `
+                    <div class="list-item">
+                        <img src="${match.image}" class="list-item-img" alt="${match.title}" />
+                        <div>
+                            <a class="list-item-title-link" href=""><h3 class="list-item-title">${match.title}</h3></a>
+                            <small class="list-item-desc">${match.path}</small>
+                        </div>
+                    </div>
+                `
+            )
+            .join('') //para converter numa string 
+        matchList.innerHTML = html;
+    }
+}
+
+// listen evento input e função callback
+search.addEventListener('focus', () => form.style.boxShadow = '0px 0px 6px 1px rgba(233, 70, 65,0.4)');
+
+search.addEventListener('input', () => {
+    iconSearch.style.visibility = 'hidden';
+    searchTshirts(search.value)
+});
